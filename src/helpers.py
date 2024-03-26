035a1623726d7c93a11735f6753f34151adb90cf
nguyenchauthaoquan <QuanNCT@outlook.com>
2024-03-26 11:10:02 +0700

    Fix merge sort comparator

--- src/helpers.py
+++ src/helpers.py
@@ -18,7 +18,7 @@ def read_csv(csv_files, delimiter=","):
         
     return contents # returns multiple contents from csv file.
 
-def merge_sort(items, order='asc'):
+def merge_sort(items, comparator=lambda x,y: x < y):
     """Sort multiple items with merge sort using divide and conquer methods
 
     Args:
@@ -37,32 +37,19 @@ def merge_sort(items, order='asc'):
         left_items = items[:mid_index] # Gets the half-left side of the list "items"
         right_items = items[mid_index:] # Gets the right-side of the list "items"
         
-        merge_sort(left_items) # Recursively call to sort the half-left side
+        merge_sort(left_items, comparator) # Recursively call to sort the half-left side
     
-        merge_sort(right_items) # Recursively call to sort the half-right side
+        merge_sort(right_items, comparator) # Recursively call to sort the half-right side
     
         left_index = right_index = main_index = 0 # Initiate the left_index for half-left side items, right_index for half-right side items and main_index for the orginal list
-        if order == 'asc': # Intiate the merge sort in ascended order.
-            while left_index < len(left_items) and right_index < len(right_items):
-                if left_items[left_index] <= right_items[right_index]:
-                    items[main_index] = left_items[left_index] # merge half-left side to the orginal list.
-                    left_index += 1
-                else:
-                    items[main_index] = right_items[right_index] # merge half-right side to the orginal list
-                    right_index += 1
-                main_index += 1
-        elif order == 'desc': # Intiate the merge sort in descended order.
-            while left_index < len(left_items) and right_index < len(right_items):
-                if left_items[left_index] >= right_items[right_index]:
-                    items[main_index] = left_items[left_index]  # merge half-left side to the orginal list.
-                    left_index += 1
-                else:
-                    items[main_index] = right_items[right_index]  # merge half-right side to the orginal list
-                    right_index += 1
-                main_index += 1
-        else:
-            raise Exception("Sorting order is not valid")    
-        
+        while left_index < len(left_items) and right_index < len(right_items):
+            if comparator(left_items[left_index],right_items[right_index]):
+                items[main_index] = left_items[left_index]  # merge half-left side to the orginal list.
+                left_index += 1
+            else:
+                items[main_index] = right_items[right_index]  # merge half-right side to the orginal list
+                right_index += 1
+            main_index += 1
         # Add the remaining items from left side to the original list.
         while left_index < len(left_items):
             items[main_index] = left_items[left_index]
